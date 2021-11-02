e4b2769632c4d6c58d825455f385918644bd7bae
DaiTran <trantrongdai308@gmail.com>
2021-11-02 19:08:52 +0700

    some artist api

--- src/main/java/com/dtrecords/dtrecords_api/api/ArtistResource.java
+++ src/main/java/com/dtrecords/dtrecords_api/api/ArtistResource.java
@@ -1,14 +1,14 @@
 package com.dtrecords.dtrecords_api.api;
 
 import com.dtrecords.dtrecords_api.domain.Artist;
+import com.dtrecords.dtrecords_api.domain.Vinyl;
 import com.dtrecords.dtrecords_api.service.ArtistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -19,6 +19,24 @@ public class ArtistResource {
 
     @GetMapping("/artists")
     public ResponseEntity<Iterable<Artist>> getArtists() {
-      return new ResponseEntity<Iterable<Artist>>(artistService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<Iterable<Artist>>(artistService.findAll(), HttpStatus.OK);
     };
+
+    @PostMapping("/admin/artist")
+    public ResponseEntity<Void> addNewArtist(@RequestParam("newArtist") String newArtist) {
+        System.out.println(newArtist);
+        artistService.save(new Artist(null,newArtist));
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/admin/artist/{id}")
+    public ResponseEntity<Artist> deleteArtist(@PathVariable Long id) {
+        Optional<Artist> artist = artistService.findById(id);
+        if (!artist.isPresent()) {
+            System.out.println("Vinyl with id " + id + " not found");
+            return new ResponseEntity<Artist>(HttpStatus.NOT_FOUND);
+        }
+        artistService.remove(id);
+        return new ResponseEntity<Artist>(HttpStatus.NO_CONTENT);
+    }
 }
