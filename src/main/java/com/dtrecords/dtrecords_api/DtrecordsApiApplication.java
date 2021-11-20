8c0ea69e542bc7950ff29e0a6f541240ce6d5f9d
DaiTran <trantrongdai308@gmail.com>
2021-11-20 09:14:50 +0700

    build jar

--- src/main/java/com/dtrecords/dtrecords_api/DtrecordsApiApplication.java
+++ src/main/java/com/dtrecords/dtrecords_api/DtrecordsApiApplication.java
@@ -1,11 +1,5 @@
 package com.dtrecords.dtrecords_api;
 
-import com.dtrecords.dtrecords_api.domain.Track;
-import com.dtrecords.dtrecords_api.domain.Vinyl;
-import com.dtrecords.dtrecords_api.service.TrackService;
-import com.dtrecords.dtrecords_api.service.UserService;
-import com.dtrecords.dtrecords_api.service.VinylService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
